3c0d94ac521be0fde1c071921425eb1de2bef364
prabesh <adhikariprabesh828@gmail.com>
2024-05-20 03:11:44 -0700

    change

---
+++ swap_with_pointer.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+void swap(int *a,int *b);
+int get_data();
+int main(){
+int a=get_data();
+int b=get_data();
+cout<<"before swap"<<endl;
+cout<<"a="<<a<<"b="<<b<<endl;
+swap(&a,&b);
+cout<<"after swap"<<endl;
+cout<<"a="<<a<<"b="<<b<<endl;
+}
+int get_data(){
+int temp;
+cout<<"enter any number";
+cin>>temp;
+return temp;
+
+}
+void swap(int *a,int *b){
+int temp=*a;
+*a=*b;
+*b=temp;
+}
